ef80b65cbd228bfd33a5914f4598477e90b734b3
MingYang-Hydrate <71815635+MingYang-Hydrate@users.noreply.github.com>
2020-10-18 20:51:42 +0800

    Update PS1_3,R.R
    I don`t think to use this tool is a good way for you to finish this work, even though it will be awesome in your research

--- PS1_3,R.R
+++ PS1_3,R.R
@@ -1,6 +1,11 @@
 Pascal_triangle <-function(k){
   n <- k-1
   s <- choose(n,k= 0:n)
+  # it is really good for a researcher to use various useful tools to improve your work efficiency
+  # but as a student, it is essential to know the basic principle，and write it by yourself
+  # moreover, this is an introductory course for programing, the most important part is to:
+  #   1   learn thinking a realistic problem with programing perspection
+  #   2   coding by yourself to solve this problem
   return(s)
 }
 Pascal_triangle(100)
